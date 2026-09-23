ffc3ed007509b9d4637b76ab7047bd9d300591ac
Akshit <134118123+akshit031@users.noreply.github.com>
2026-09-23 15:35:20 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int i, int n, vector<int>& v, vector<int>& dp){
+        if(i > n)return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = v[i] + f(i+2, n, v, dp);
+        int notake = f(i+1, n, v, dp);
+        return dp[i] = max(take, notake);
+    }
+    int rob(vector<int>& v) {
+        int n = v.size();
+        if(n == 1) return v[0];
+        vector<int> dp(n, -1);
+        int one = f(0, n-2, v, dp);
+        for(auto& val:dp) val = -1;
+        int two = f(1, n-1, v, dp);
+        return max(one, two);
+    }
+};
